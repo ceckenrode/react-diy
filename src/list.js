9c10d9b126306519151de93d4eca842e260d2613
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-04-24 12:39:47 -0400

    added input to checklist component

--- src/list.js
+++ src/list.js
@@ -6,6 +6,7 @@ class List extends Component {
     var cards = this.props.cards.map((card) => {
       return <Card id={card.id} title={card.title}
         description={card.description}
+        color={card.color}
         tasks={card.tasks} />
     });
     return (
